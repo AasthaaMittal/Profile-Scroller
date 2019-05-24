2f462a7faee49c1f59545ae57b92aeebb9833ecf
AasthaaMittal <39242283+AasthaaMittal@users.noreply.github.com>
2019-05-24 15:17:05 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,52 @@
+ const data = [
+     {
+         name: 'John Doe',
+         age: 30;
+         gender: 'male',
+         location: 'Boston', 
+         image: 'https://randomuser.me/api/portraits/men/32.jpg'
+     },
+     {
+         name: 'Jen Swift',
+         age: 26;
+         gender: 'female',
+         location: 'Miami',
+         image: 'https://randomuser.me/api/portraits/women/28.jpg'
+     },
+     {   name: 'William Smith',
+         age: 36;
+         gender: 'male',
+         location: 'Lynn',
+         image: 'https://randomuser.me/api/portraits/men/83.jpg'
+     }
+ ]
+ 
+const profiles = profileIterator(data);
+
+document.getElementById('next').addEventListener('click', nextProfile);
+
+//Event listener
+function nextProfile(){
+    let currentProf = profiles.next.value;
+    document.getElementById('imgDisplay').innerHTML = `<img src=${currentProf.image}>`;
+    
+    document.getElementById('profileDisplay').innerHTML = `
+        <ul class = "list-group">
+            <li class = "list-group-item">Name : ${currentProf.name}</li>
+            <li class = "list-group-item">Age : ${currentProf.age}</li>
+            <li class = "list-group-item">Gender : ${currentProf.gender}</li>
+            <li class = "list-group-item">Location : ${currentProf.location}</li>
+        </ul>
+    `;
+}
+//Profile Iterator
+function profileIterator(profiles){
+    let nextIndex = 0;
+    
+    return{
+        next: function(){
+            return nextIndex++ < profiles.length ? { value: profiles[nextIndex], done: false} :  { done: true};
+            }
+
+    }
+}
